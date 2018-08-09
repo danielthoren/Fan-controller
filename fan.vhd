ec52d68dfc8125beec453014ed7d29c01d6bb2ce
danielthoren <danne_thoren456@hotmail.com>
2018-08-09 09:09:07 +0200

    Work on top level file 'fan controller', now compiles but not done Added component fan and internal signals for fan, also created the first instance of fan and its port map. Changed sensativity lists inside 'fan' entity as well

--- fan.vhd
+++ fan.vhd
@@ -42,66 +42,61 @@ end fan;
 
 architecture Behavioral of fan is
 
-signal pwm_counter: 	unsigned(4 downto 0) := (others=>'0');	--Used for PWM output, counts to 20 then restarts
-signal pin_4: 		std_logic := '0';			--True if the connected fan is of 4-pin type
+	signal pwm_counter: 	unsigned(4 downto 0) := (others=>'0');	--Used for PWM output, counts to 20 then restarts
+	signal pin_4: 		std_logic := '0';			--True if the connected fan is of 4-pin type
 
-signal tacho_counter: 	unsigned(7 downto 0) := (others=>'0');	--Counts amount of pulses from fan tachometer during halv a second
+	signal tacho_counter: 	unsigned(7 downto 0) := (others=>'0');	--Counts amount of pulses from fan tachometer during halv a second
 
 begin
 
---Process block that measures the amount of times the tachometer pulses during a second. Since there are two pulses per
---revolution it is enough to measure a halv second (0.5) and use measured value as rotations per whole second. 
---This is used to calculate the speed of the fan in RPM on the computer side. 
---@clk: The clocksignal driving the reset functionality
---@half_sec_clk: The clock at which the counter value is written to the output 'pulses_sec' (half a second)
---@rst: Resets the count
---@tacho: The tachometer signal (square wave)
---@tacho_counter: The counter that counts the tacho pulses
-tacho_count: process(clk, half_sec_clk, rst, tacho, tacho_counter)
-begin
-	if rising_edge(clk) then
-		if rst = '1' then
-			tacho_counter <= (others=>'0');
-			pulses_sec <= (others=>'0');
-		end if;
-	end if;
-
-	--Not synchronized by 'clk' since 'half_sec_clk' is synchronized already
-	if rising_edge(half_sec_clk) then
-		pulses_sec <= std_logic_vector(tacho_counter);
-		tacho_counter <= (others=>'0');
-	elsif rising_edge(tacho) then
-		tacho_counter <= tacho_counter + 1;
-	end if;
-end process;
-
---Process block that generates the PWM output.
---@clk: The clocksignal driving the pwm generator process. It must be (25 * 20)kHz (+- 3kHz) thus generating a
---			 PWM frequency of between 21kHz and 28kHz.
---@rst: Resets pwm driver.
---@pwm_counter: 5-bit counter used to generate pwm signal.
---@duty_cycle: 5-bit 'std_logic_vector' holding a binary value between 0 and 20 (unsigned value)
-pwm: process(clk, rst)
-begin
-	if rising_edge(clk) then
-		pwm_counter <= pwm_counter + 1;
-		--Reset if sig 'rst' goes high
-		if rst = '1' then
-			pwm_counter <= (others=>'0');
-			pwm_signal <= '0';
-		end if;
-		
-		if pwm_counter = unsigned(duty_cycle) then
-			pwm_signal <= '0';
-		end if;
-			
-		if pwm_counter = "10100" then 
-			if not(unsigned(duty_cycle) = 0) then
-			 	pwm_signal <= '1';
+	--Process block that measures the amount of times the tachometer pulses during a second. Since there are two pulses per
+	--revolution it is enough to measure a halv second (0.5) and use measured value as rotations per whole second. 
+	--This is used to calculate the speed of the fan in RPM on the computer side. 
+	--@clk: The clocksignal driving the reset functionality
+	--@half_sec_clk: The clock at which the counter value is written to the output 'pulses_sec' (half a second)
+	tacho_count: process(clk, half_sec_clk, tacho)
+	begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				tacho_counter <= (others=>'0');
+				pulses_sec <= (others=>'0');
 			end if;
-			pwm_counter <= (others=>'0');
 		end if;
 	
-	end if;	
-end process;
+		--Not synchronized by 'clk' since 'half_sec_clk' is synchronized already
+		if rising_edge(half_sec_clk) then
+			pulses_sec <= std_logic_vector(tacho_counter);
+			tacho_counter <= (others=>'0');
+		elsif rising_edge(tacho) then
+			tacho_counter <= tacho_counter + 1;
+		end if;
+	end process;
+
+	--Process block that generates the PWM output.
+	--@clk: The clocksignal driving the pwm generator process. It must be (25 * 20)kHz (+- 3kHz) thus generating a
+	--			 PWM frequency of between 21kHz and 28kHz.
+	--@rst: Resets pwm driver.
+	pwm: process(clk, rst)
+	begin
+		if rising_edge(clk) then
+			pwm_counter <= pwm_counter + 1;
+			--Reset if sig 'rst' goes high
+			if rst = '1' then
+				pwm_counter <= (others=>'0');
+				pwm_signal <= '0';
+			end if;
+			
+			if pwm_counter = unsigned(duty_cycle) then
+				pwm_signal <= '0';
+			end if;
+				
+			if pwm_counter = "10100" then 
+				if not(unsigned(duty_cycle) = 0) then
+				 	pwm_signal <= '1';
+				end if;
+				pwm_counter <= (others=>'0');
+			end if;
+		
+		end if;	
+	end process;
 end Behavioral;
--- fan_controller.vhd
+++ fan_controller.vhd
@@ -9,8 +9,8 @@ entity fan_controller is
 			rst:		in std_logic;
 			
 			--Fan io section
-			tacho:			in std_logic_vector(7 downto 0);	--tachometer input from all 8 fans
-			pwm_sig:		out std_logic_vector(7 downto 0);	--pwm output for all 8 fans
+			i_fans_tacho:		in std_logic_vector(7 downto 0);	--tachometer input from all 8 fans
+			o_fans_pwm_sig:		out std_logic_vector(7 downto 0);	--pwm output for all 8 fans
 
 			--Uart io
 			i_rx_serial:		in std_logic;
@@ -20,4 +20,64 @@ end fan_controller;
 
 architecture behave of fan_controller is
 
-	--
+	signal uart_clk:		std_logic;		--clock for uart logic (6MHz / 5 = 1.2MHz)
+	signal uart_clk_counter:	unsigned(2 downto 0);	--counter for uart divider
+	signal half_sec_clk:		std_logic;		--clock with half sec period (2Hz)
+	signal half_sec_counter:	unsigned(20 downto 0);	--counter for clock divider
+
+	signal fans_pwm:		std_logic_vector(7 downto 0);	--internal fans pwm output signal
+	signal fans_duty_cycle:		std_logic_vector(39 downto 0);	--internal fans duty cycle register (5 bits per fan, decimal value 0-21 representing 0-100%)
+	signal fans_pulses_sec:		std_logic_vector(63 downto 0);	--internal fans speed in rotations per seconds
+
+component fan is
+	port(
+			clk: 			in std_logic;				--clock must be 500kHz for correct pwm frequency (25kHz)
+			rst: 			in std_logic;				--Reset
+			half_sec_clk: 		in std_logic;				--Clock with 2Hz rate (high flank every 0.5 seconds). Used to measure speed
+			tacho: 			in std_logic;				--Tachometer input
+			duty_cycle: 		in std_logic_vector(4 downto 0);	--Dutycycle of pwm, decimal value between 0 and 20 (unsigned)
+
+			pwm_signal: 		out std_logic;				--PWM output
+			pulses_sec: 		out std_logic_vector(7 downto 0)	--Pulses per second, used to calculate fan speed (RPM = pulses_sec * 60)
+		);
+end component;
+	
+
+begin
+
+	--declaring all fans
+	fan_0: fan
+		port map(
+			clk => clk,
+			rst => rst,
+			half_sec_clk => half_sec_clk,
+			tacho => i_fans_tacho(0),
+			duty_cycle => fans_duty_cycle(4 downto 0),
+			pwm_signal => o_fans_pwm_sig(0),
+			pulses_sec => fans_pulses_sec(7 downto 0)
+			);
+
+	--Process block handles clk divider logic
+	clk_divider: process(clk)
+	begin
+		if rising_edge(clk) then
+			uart_clk_counter <= uart_clk_counter + 1;
+			if uart_clk_counter = 5 then
+				uart_clk_counter <= (others=>'0');	--incrementing uart counter
+				if uart_clk = '0' then
+					uart_clk <= '1';
+					half_sec_counter <= half_sec_counter + 1;	--incrementing half sec divider on high flank of uart_clk (saving registers)
+				else
+					uart_clk <= '0';
+					
+				end if;
+			end if;
+
+		end if;
+	end process;
+
+	
+
+	
+
+end behave;
